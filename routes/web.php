f9badbd938be96a51f2feb58ed7008dfa8436a2a
Diego-Garcia-GG <123047970@upq.edu.mx>
2025-10-15 15:03:57 -0600

    MVC de Division

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\calculadora;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/division/{num1}/{num2}',[calculadora::class,'division']);
