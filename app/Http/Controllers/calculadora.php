fbc30d0bec0db3ae8686e2dab4a65ae544506f1f
Angelsan22 <123048340@upq.edu.mx>
2025-10-15 15:04:00 -0600

    MVC de la multiplicación

--- app/Http/Controllers/calculadora.php
+++ app/Http/Controllers/calculadora.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class calculadora extends Controller
 {
-    //
+    public function multiplicacion($num1, $num2)
+    {
+        $resultado = $num1 * $num2;
+        return view('calculadora.multiplicacion', ['num1' => $num1, 'num2' => $num2, 'resultado' => $resultado]);
+    }
+
+
 }
